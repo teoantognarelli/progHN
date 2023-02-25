17f13c04dbc59afdfa706ea9da69a1b5ada6b24e
teoantognarelli <75087755+teoantognarelli@users.noreply.github.com>
2023-02-25 19:32:10 +0100

    Add files via upload

---
+++ assets/JS/devoirstring.js
@@ -0,0 +1,21 @@
+// exercice 1
+function prenom() {
+    // on récupère le prénom on le met dans l'élément avec id holder1
+    document.getElementById("prenom()")innerHTML="Teo";
+
+function nomdefamille() {
+    // on récupère le nom on le met dans l'élément avec id holder1
+    // compléter le code ici
+}
+
+function nomcomplet() {
+    // on concatène prénom et nom pour afficher le nom entier et on met le résultat dans l'élément avec id holder1
+    // compléter le code ici
+}
+
+
+// exercice 2
+function segmentText() {
+    // on récupère le texte de l'élément d'id texte, on le découpe et on le place dans l'élément avec id holder2
+    // compléter le code ici
+}
