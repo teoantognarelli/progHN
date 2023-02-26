30dc26a4c18f38d7d7cf8a12e4e6a453fc58a22b
teoantognarelli <75087755+teoantognarelli@users.noreply.github.com>
2023-02-26 20:15:54 +0100

    Add files via upload

--- assets/JS/devoirstring.js
+++ assets/JS/devoirstring.js
@@ -1,21 +1,31 @@
 // exercice 1
 function prenom() {
     // on récupère le prénom on le met dans l'élément avec id holder1
-    document.getElementById("prenom()")innerHTML="Teo";
+    var input=document.getElementById("prenom").inholder1;
+    alert(input);
 
 function nomdefamille() {
     // on récupère le nom on le met dans l'élément avec id holder1
-    // compléter le code ici
+    var input=document.getElementById("nomdefamille").value;
+    alert(input);// compléter le code ici
 }
 
 function nomcomplet() {
     // on concatène prénom et nom pour afficher le nom entier et on met le résultat dans l'élément avec id holder1
-    // compléter le code ici
+    let prenom()= "prenom";
+    let nomdefamille()="nomdefamille";
+    let nomcomplet()= prenom().concat("",nomdefamille());
+
+    var nomcomplet()=document.getElementById("nomcomplet()");
+    alert (input)// compléter le code ici
 }
 
 
 // exercice 2
 function segmentText() {
     // on récupère le texte de l'élément d'id texte, on le découpe et on le place dans l'élément avec id holder2
+    var =document.getElementById("texte").value;
+    alert(value)
+    let "texte"= text split([""])
     // compléter le code ici
 }
