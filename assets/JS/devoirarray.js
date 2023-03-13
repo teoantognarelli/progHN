09e7ede2ccf172e6e01eac8b94ae81836772cd28
teoantognarelli <75087755+teoantognarelli@users.noreply.github.com>
2023-03-13 20:39:35 +0100

    Add files via upload

--- assets/JS/devoirarray.js
+++ assets/JS/devoirarray.js
@@ -2,26 +2,26 @@ function exercice1() {
 	let fruits = ['Pomme', 'Poire', 'Ananas'];
 	let last = fruits.pop(); // supprime Ananas à la fin
 	let first = fruits.unshift('Ananas'); //mettre Ananas au début de la liste
-	document.getElementById("exercice1Resultat")innerHTML="fruits";
+	document.getElementById("exercice1Resultat")innerHTML=fruits;
 	
 }
 
 
 function exercice2() {
 	let mots = ["Ici", ",", "on", "entre", "du", "texte", "qu", "'", "on", "va", "segmenter", "en", "mots", "."];
-	document.getElementById("exercice2Resultat").innerHTML="mots".join("\s");
+	document.getElementById("exercice2Resultat").innerHTML=mots.join("\s");
 
-	document.getElementById("exercice2Resultat").innerHTML= "mots".toUpperCase();
+	document.getElementById("exercice2Resultat").innerHTML= mots.toUpperCase();
 }
 
 
 function exercice3() {
 	let mots = ["Ici", ",", "on", "entre", "du", "texte", "qu", "'", "on", "va", "segmenter", "en", "mots", "."];
-	document.getElementById("exercice3Resultat").innerHTML="mots".join("\s");
+	document.getElementById("exercice3Resultat").innerHTML=mot.join("\s");
 	if (mots <= 3) {
 		delete mot
 	};//Découpez le texte suivants en mots (selon des espaces) puis retirer les mots qui ont une taille de 3 ou moins.
-	document.getElementById("exercice3Resultat").innerHTML= "mots";
+	document.getElementById("exercice3Resultat").innerHTML= mots;
 }
 
 
